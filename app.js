39b856107a36183de4a77185b1f6ec96a60fc8cd
mahdi <mahdimamashli1383@gmail.com>
2024-01-08 17:54:10 +0330

    Update app.js: Remove unnecessary lines.

--- app.js
+++ app.js
@@ -12,16 +12,14 @@ app.set("view engin", "ejs");
 app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer);
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/public/image",express.static(path.join(__dirname, "public", "images")));
 app.use(session);
 app.use(flash());
 const csrfProtection = csurf();
 app.use(csrfProtection);
-const URL = "mongodb://localhost:27017/BaseShop";
 app.use(locals);
-
+const URL = "mongodb://localhost:27017/BaseShop";
 const start = async () => {
   try {
     await mongoose.connect(URL);
