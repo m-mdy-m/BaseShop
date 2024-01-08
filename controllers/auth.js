85e8020d3dab4f759ee1d7de2a7540e2d6670baf
mahdi <mahdimamashli1383@gmail.com>
2024-01-08 21:12:26 +0330

    Implement user sign up functionality and add error handling.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,52 @@
 const render = require("../util/render");
-const User = require("../models/User")
-const { validationResult } = require('express-validator')
+const User = require("../models/User");
+const { validationResult } = require("express-validator");
+const bcryptjs = require("bcryptjs");
 exports.getSignUp = (req, res, nxt) => {
   const msgErr = req.flash("ERROR");
-  render(req, res, "auth/signup", "SIGNUP", msgErr, []);
+  render(req, res, "auth/signup", "SIGNUP", msgErr, [], {
+    oldValue: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 };
-exports.postSignUp =async (req,res,nxt)=>{
-  const name = req.body.name
-  const email = req.body.email
-  const password = req.body.password
-  const err = validationResult(req)
-  if(!err.isEmpty()){
-    let errors = err.array()
-    console.log('errors=>',errors)
-    return render(req,res,'auth/signUp',"SIGNUP", err.array()[0].msg, errors,)
+exports.postSignUp = async (req, res, nxt) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  const err = validationResult(req);
+  if (!err.isEmpty()) {
+    let errors = err.array();
+    console.log("errors=>", errors);
+    return render(
+      req,
+      res,
+      "auth/signUp",
+      "SIGNUP",
+      err.array()[0].msg,
+      errors,
+      {oldValue : {
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      }}
+    );
   }
-  
-}
+  const hashedPassword = await bcryptjs.hash(password, 12);
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    cart: [],
+  });
+  await user.save();
+  req.session.isLogin = true;
+  req.session.user = user;
+  req.session.save();
+  res.redirect("/");
+};
--- util/render.js
+++ util/render.js
@@ -1,16 +1,29 @@
-const render = (req, res, view, title, msgErr = null, validateErrors = [], ...newObj) => {
+const render = (
+  req,
+  res,
+  view,
+  title,
+  msgErr = null,
+  validateErrors = [],
+  { ...newObj }
+) => {
   let messageError = req.flash("error");
   if (messageError.length > 0) {
     messageError = messageError[0];
   } else {
     messageError = null;
   }
+  var ErrorPath;
+  validateErrors.forEach((e) => {
+    console.log("e =>", e.path);
+    ErrorPath = e.path;
+  });
   res.render(view, {
     title: title,
     path: req.path,
     msgErr: messageError,
-    validateErrors: validateErrors,
-    ...newObj
+    validateErrors: ErrorPath,
+    ...newObj,
   });
 };
 module.exports = render;
