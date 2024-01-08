7cc9f2d7efc601e815c2c052ea72158e062d4ba9
mahdi <mahdimamashli1383@gmail.com>
2024-01-08 23:37:34 +0330

    Update logout functionality and remove unnecessary code.

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,14 @@
 const logout = async (event) => {
   const csrf = event.parentNode.querySelector("[name =_csrf]").value;
-  console.log(csrf)
-//   const result = await fetch("/logout", {
-//     method: "DELETE",
-//     headers: {
-//       "csrf-token": csrf,
-//     },
-//     credentials: 'same-origin'
-
-//   });
-//   await result.json()
+  console.log(csrf);
+  const url = "/logout";
+  const result = await fetch(url, {});
+  console.log(result);
+  //   const result = await fetch("/logout", {
+  //     method: "DELETE",
+  //     headers: {
+  //       "csrf-token": csrf,
+  //     },
+  //   });
+  //   await result.json()
 };
