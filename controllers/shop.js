84b505ecb1b18978bb9c27a9939f193c291e998e
mahdi <mahdimamashli1383@gmail.com>
2024-01-09 23:50:06 +0330

    Add products feature

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,9 +1,10 @@
 const render = require("../util/render");
 const Product = require("../models/Product");
-const User = require('../models/User')
+const User = require("../models/User");
 const { validationResult } = require("express-validator");
-exports.getShop = (req, res, nxt) => {
-  render(req, res, "shop/index", "HOME");
+exports.getShop = async (req, res, nxt) => {
+  const products = await Product.find();
+  render(req, res, "shop/index", "HOME", null, [], { products });
 };
 exports.getAddProduct = (req, res, nxt) => {
   render(req, res, "shop/add-product", "ADD_PRODUCT", null, [], {
@@ -16,32 +17,32 @@ exports.postAddProduct = async (req, res, nxt) => {
   const image = req.file;
   const price = req.body.price;
   const err = validationResult(req);
-  const user = req.user
-  const imagePath = image.path
-  if(!err.isEmpty()){
-    let errors = err.array()
-    const msgErr = err.array()[0].msg
-    return render(req,res,'shop/add-product',"ADD PRODUCT",msgErr,errors,{
-      edit : false,
-      hasError : true,
-      product:{
+  const user = req.user;
+  const imagePath = image.path;
+  if (!err.isEmpty()) {
+    let errors = err.array();
+    const msgErr = err.array()[0].msg;
+    return render(req, res, "shop/add-product", "ADD PRODUCT", msgErr, errors, {
+      edit: false,
+      hasError: true,
+      product: {
         title: title,
-        price : price,
-      }
-    })
+        price: price,
+      },
+    });
   }
-  const userId = await User.findById(user._id)
-  if(!userId){
-    return render(req,res,'shop/index','add-product', 'HAS NOT USER FOUND' )
+  const userId = await User.findById(user._id);
+  if (!userId) {
+    return render(req, res, "shop/index", "add-product", "HAS NOT USER FOUND");
   }
   const product = await Product.create({
     title,
     imagePath,
     price,
     userId,
-    username : user.name
-  })
-  await product.save()
-  console.log("CREATE NEW PRODUCT ")
-  res.redirect('/')
+    username: user.name,
+  });
+  await product.save();
+  console.log("CREATE NEW PRODUCT ");
+  res.redirect("/");
 };
