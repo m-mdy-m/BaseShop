21a9f90086921660ce6fc88d0d4a1be95d1860f8
mahdi <mahdimamashli1383@gmail.com>
2024-01-09 21:18:33 +0330

    create module sendEmail

---
+++ util/sendEmail.js
@@ -0,0 +1,18 @@
+const nodemailer = require("nodemailer");
+let transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7d555598001454",
+    pass: "745c69e3e0c996",
+  },
+});
+const sendEmail = (email,token)=>{
+    transport.sendMail({
+        from : "mahdimamashli1383@gmail.com",
+        to : email,
+        subject : "RESET PASSWORD",
+        html : `<a href=http://localhost:3000/reset/${token}></a>`
+    })
+}
+module.exports = sendEmail
