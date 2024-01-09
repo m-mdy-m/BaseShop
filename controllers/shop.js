c5b46e45b52b533698c9437bfe60fe16f345c346
mahdi <mahdimamashli1383@gmail.com>
2024-01-09 23:06:08 +0330

    import Product model and add get and post controllers

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,31 @@
 const render = require("../util/render");
-
+const Product = require("../models/Product");
+const { validationResult } = require("express-validator");
 exports.getShop = (req, res, nxt) => {
   render(req, res, "shop/index", "HOME");
+};
+exports.getAddProduct = (req, res, nxt) => {
+  render(req, res, "shop/add-product", "ADD_PRODUCT", null, [], {
+    edit: false,
+    hasError: false,
+  });
+};
+exports.postAddProduct = async (req, res, nxt) => {
+  const title = req.body.title;
+  const image = req.file;
+  const price = req.body.price;
+  const err = validationResult(req);
+  const imagePath = image.path
+  if(!err.isEmpty()){
+    let errors = err.array()
+    const msgErr = err.array()[0].msg
+    return render(req,res,'shop/add-product',"ADD PRODUCT",msgErr,errors,{
+      edit : false,
+      hasError : true,
+      product:{
+        title: title,
+        price : price,
+      }
+    })
+  }
 };
