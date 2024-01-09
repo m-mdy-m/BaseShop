7432127f510deb4b603ff8b82911823063b223b6
mahdi <mahdimamashli1383@gmail.com>
2024-01-09 23:05:19 +0330

    add get and post addproduct  route

--- routes/shop.js
+++ routes/shop.js
@@ -1,4 +1,7 @@
 const router = require("../util/router");
 const shopControl = require("../controllers/shop");
+const isAuth = require('../middleware/is-Auth')
 router.get("/", shopControl.getShop);
+router.get('/add-product', isAuth , shopControl.getAddProduct)
+router.post('/add-product', isAuth, shopControl.postAddProduct)
 module.exports = router
