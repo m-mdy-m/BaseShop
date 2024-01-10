80bcd62c45827726aadfc3acd818e2ed92593b9d
mahdi <mahdimamashli1383@gmail.com>
2024-01-10 21:01:50 +0330

    update

--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,6 @@ const isAuth = require('../middleware/is-Auth')
 const adminControl = require('../controllers/admin')
 router.get('/admin',isAuth,adminControl.getAdmin)
 
+
+router.delete('/delete-product', isAuth, adminControl.deleteProduct)
 module.exports = router
