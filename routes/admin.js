3f39db068c9eaf919ec9c9ee874ba2986694b41f
mahdi <mahdimamashli1383@gmail.com>
2024-01-10 23:16:08 +0330

    add get and put route edit

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,6 @@ router.get('/admin',isAuth,adminControl.getAdmin)
 
 
 router.delete('/delete-product/:prodId', isAuth, adminControl.deleteProduct)
+router.get('/edit-product/:prodId', isAuth, adminControl.getEdit)
+router.put('/edit-product',isAuth,adminControl.EditProduct)
 module.exports = router
