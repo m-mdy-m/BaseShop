aa2212c1d16b2708a0278110a22177ed68fbd0c6
mahdi <mahdimamashli1383@gmail.com>
2024-01-10 20:15:26 +0330

    Update product pagination logic and UI.

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,26 +3,20 @@ const Product = require("../models/Product");
 const User = require("../models/User");
 const { validationResult } = require("express-validator");
 
-const NUMBER_PRODUCT = 2;
+const NUMBER_PRODUCT = 3
 exports.getShop = async (req, res, nxt) => {
-  const page = +req.query.page || 1;
-  const nap = await Product.countDocuments(); // ** NAP => number all product
-
-  const products = await Product.find()
-    .skip((page - 1) * NUMBER_PRODUCT)
-    .limit(NUMBER_PRODUCT);
-  console.log('1 =>',NUMBER_PRODUCT * page < nap);
-  console.log('2 =>',page > 1);
-  console.log('3 =>',page + 1);
-  console.log('4 =>',Math.ceil(nap / NUMBER_PRODUCT));
+  const page = +req.query.page || 1
+  const totalItem = await Product.countDocuments()
+  const products = await Product.find().skip((page - 1) * NUMBER_PRODUCT).limit(NUMBER_PRODUCT)
+  console.log('=>', Math.ceil(totalItem / NUMBER_PRODUCT))
   render(req, res, "shop/index", "HOME", null, [], {
     products,
-    page,
-    hasNxtPage: NUMBER_PRODUCT * page < nap,
-    hasPrevPage: page > 1,
-    nxtPage: page + 1,
-    prevPage: page - 1,
-    lastPage: Math.ceil(nap / NUMBER_PRODUCT),
+    page : page,
+    prevPage : page - 1,
+    hasPrevPage : page > 1,
+    hasNxtPage :  page * NUMBER_PRODUCT < totalItem,
+    nxtPage : page + 1,
+    lastPage : Math.ceil(totalItem / NUMBER_PRODUCT)
   });
 };
 exports.getAddProduct = (req, res, nxt) => {
